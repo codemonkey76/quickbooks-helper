d6edf5ee978ac809a90e0e637e0b2f6831e0707b
Shane Poppleton <shane@bjja.com.au>
2022-03-23 11:20:44 +1000

    Add middleware and update README.md

--- src/Http/Middleware/QuickbooksConnected.php
+++ src/Http/Middleware/QuickbooksConnected.php
@@ -2,22 +2,42 @@
 
 namespace Codemonkey76\Quickbooks\Http\Middleware;
 
-use Illuminate\Contracts\Routing\UrlGenerator;
-use Illuminate\Contracts\Session\Session;
+use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
-use Spinen\QuickBooks\Client;
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Contracts\Routing\UrlGenerator;
+use Codemonkey76\Quickbooks\QuickbooksClient;
 
 class QuickbooksConnected
 {
-    protected $quickbooksClient;
-    protected $redirector;
-    protected $session;
-    protected $url_generator;
+    protected QuickbooksClient $quickbooksClient;
+    protected Redirector $redirector;
+    protected Session $session;
+    protected UrlGenerator $url_generator;
 
-    public function __construct()
+    public function __construct(
+        QuickbooksClient $quickbooksClient,
+        Redirector $redirector,
+        Session $session,
+        UrlGenerator $url_generator)
     {
-        
+        $this->quickbooksClient = $quickbooksClient;
+        $this->redirector = $redirector;
+        $this->session = $session;
+        $this->url_generator = $url_generator;
     }
-    
+
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$this->quickbooksClient->hasValidRefreshToken())
+        {
+            $this->session->put('url.intended', $this->url_generator->to($request->path()));
+
+            return $this->redirector->route('quickbooks.connect');
+        }
+
+        return $next($request);
+    }
+
 }
